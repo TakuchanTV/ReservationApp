4654b919d88dbf4a49e52524aa7d057d9d030018
TakuchanTV <fk.53rk2@icloud.com>
2024-07-04 21:12:19 +0900

    ✨ transformを使ってカレンダーの位置を変更した。

--- my-app/src/Components/CalenderCss.jsx
+++ my-app/src/Components/CalenderCss.jsx
@@ -1,1 +1,14 @@
-import CalendarPage from "./Calender";
+//CalenderCss.jsx//
+
+import Calendar from "react-calendar";
+import styled from "styled-components";
+
+export const StyledCalender = styled(Calendar)`
+width: 30%;
+max-width: 500px;
+transform: translateY(80px);
+
+
+`
+
+
