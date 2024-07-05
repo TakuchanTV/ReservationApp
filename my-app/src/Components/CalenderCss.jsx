f4cf7c408e058240f81dfd4b39d77679fe7f6986
TakuchanTV <fk.53rk2@icloud.com>
2024-07-05 22:04:58 +0900

    📝 CSSを当てたが、反映されていない。ショック。

--- my-app/src/Components/CalenderCss.jsx
+++ my-app/src/Components/CalenderCss.jsx
@@ -6,7 +6,14 @@ import styled from "styled-components";
 export const StyledCalender = styled(Calendar)`
 width: 30%;
 max-width: 500px;
-transform: translateY(80px);
+transform: translateY(100px);
+text-align: center;
+.fc.fc-col-header-cell{
+    font-size: 0.75rem;
+    font-weight: normal;
+   color :#b6b5b3 ;
+   border: none;
+}
 
 
 `
