710c63a4a929b56c622ad3086f96d783e060efb9
TakuchanTV <fk.53rk2@icloud.com>
2024-07-06 15:09:17 +0900

    📝 marginによってボタンの位置を微調整した。

--- my-app/src/Components/CalenderCss.jsx
+++ my-app/src/Components/CalenderCss.jsx
@@ -6,8 +6,9 @@ import styled from "styled-components";
 export const StyledCalender = styled(Calendar)`
 width: 30%;
 max-width: 500px;
-transform: translateY(100px);
+transform: translateY(150px);
 text-align: center;
+
 
 .react-calendar__month-view__days__day--weekend:nth-child(7n-1){
     color: blue;
@@ -17,7 +18,23 @@ text-align: center;
     color: red;
 }
 
+
 `
 
+export const Sdiv = styled.div`
+display: flex ;
+justify-content: center;
+align-items: flex-end;
+margin-top: 20px;
+margin-right: 850px;
 
+`
+
+export const Sbutton = styled.button`
+   transform :translateY(150px) ;
+   margin: 5px;
+   :hover{
+    
+   }
+`
 
