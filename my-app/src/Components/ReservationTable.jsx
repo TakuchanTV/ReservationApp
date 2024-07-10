0e272bcab00760f7a6d234565cd84403153f97c6
TakuchanTV <fk.53rk2@icloud.com>
2024-07-11 02:42:19 +0900

    📝 予約のテーブルを作りました。

---
+++ my-app/src/Components/ReservationTable.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ReservationTable = () => {
+    return(
+      <>
+      <div>
+        <table>
+          <tr>1</tr><tr>2</tr>
+        </table>
+      </div>
+      </>  
+    )
+}
